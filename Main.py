fbe6451ea6101065a191b8456e2049c80f549a8c
agnespaulurk21cs1020 <109952516+agnespaulurk21cs1020@users.noreply.github.com>
2022-10-03 15:10:54 +0530

    second commit

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 from typing import List
 
 def merge_sort(data) -> None:
- def merge_Sort(myList) -> None:
+ def merge_sort(myList) -> None:
   if len(myList) > 1:
         mid = len(myList) // 2
         left = myList[:mid]
